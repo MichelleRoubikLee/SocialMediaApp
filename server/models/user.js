f8f8686d0fbe55af638bc1626cf471265cbbc849
Your Name <youremail@yourdomain.com>
2021-01-13 19:08:46 -0500

    mongo created and operational

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,19 @@
+// const { number } = require('joi');
 const mongoose = require('mongoose');
-const Joi = require('joi');
+// const Joi = require('joi');
 
 //user schema goes here
+const userSchema = new mongoose.Schema({
+    userId: {type: Number, required: true},
+    name: {type: String, required: true, minlength: 5, maxlength: 255 },
+    joinDate: {type: Date, default: Date.now()},
+    // friends: [userSchema]
+});
 
+const User = mongoose.model('user',userSchema);
 //validation goes here
 
 //export goes here
+
+exports.User = User;
+exports.userSchema = userSchema;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,39 @@
-const {User,validate} = require('../models/user');
-const express = require('express');
+const { User, validate } = require("../models/user");
+const express = require("express");
 const router = express.Router();
 
 //endpoints and route handlers go here
 
-module.exports = router; 
+//get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.send(users);
+  } catch (ex) {
+    return res.status(500).send(`Internal server Error: ${ex}`);
+  }
+});
+
+//put new user
+router.post('/new', async (req,res) => {
+    try {
+        // const{error}=validate(req.body);
+        // if (error)
+        //     return res.status(400).send(error);
+
+        const user = new User ({
+            userId: req.body.userId,
+            name: req.body.name,
+            joinDate: req.body.joinDate,
+        });
+
+        await user.save();
+        
+        return res.send(user);
+    } catch (ex) {
+        return res.status(500).send(`InternalServerError:${ex}`);
+    }
+});
+
+module.exports = router;
+
