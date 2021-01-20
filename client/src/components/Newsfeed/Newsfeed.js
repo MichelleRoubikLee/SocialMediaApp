6ed6fba129ba98e8658abd51702bcef85b2870d5
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-01-20 17:10:11 -0600

    added useEffect to NewsFeed

--- client/src/components/Newsfeed/Newsfeed.js
+++ client/src/components/Newsfeed/Newsfeed.js
@@ -1,38 +1,43 @@
-
-import React, { useState }from 'react';
+import React, { useEffect, useState }from 'react';
 import axios from 'axios';
 import {API_BASE_URL} from '../../config/config.js';
-import { PromiseProvider } from 'mongoose';
-import { array } from 'joi';
+import CommentBox from "./CommentBox/commentBox";
+
+
 
 
 
 function NewsFeed (props) {
 
-    // const [login, setLogin] = useState({
-    //     email: "",
-    //     password: ""
+    const [allComments, setAllComments] = useState()
+
+
+
+    // useEffect(() => {
+    //     const newurl = API_BASE_URL;
+    //     axios({
+    //         method: 'get',
+    //         url: newurl,
+    //     }).then((res) => {
+    //         allComments = res.data
+    //         setAllComments(
+                
+    //         )
+    //         console.log(allComments)
+    //     })
     // })
-
-
-
-    function updateComments() {
-        const newurl = API_BASE_URL;
-        axios({
-            method: 'get',
-            url: newurl,
-        }).then(
-
-        )
-    }
+    // function getAllComments() {
+       
+    // }
 
     return (
         <div className = "NewsFeed">
-            {/* <div>
-                {props.comments.map((comment, index) => (
-                    <Comment key={index} comment={comment} />
-                ))}
-            </div> */}
+            <div>
+                {/* {getAllComments()}
+                {allComments.map((comment, index) => (
+                    <CommentBox key={index} comment={comment} />
+                ))} */}
+            </div>
         </div>
     )
 }
