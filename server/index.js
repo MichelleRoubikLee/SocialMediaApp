7ea0483c9215dd92c7902ad658ebf04763da6562
brooke <daisyrakionvideos@gmail.com>
2021-01-21 19:19:28 -0600

    profilepicture on client side is done. just no axios request yet.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 const users = require('./routes/users');
 const cors = require('cors');
 const auth = require('./routes/auth');
@@ -15,7 +16,9 @@ app.use('/api/auth', auth); //not sure if this is formed right
 app.use('/api/newinfo', petinfo);
 
 
-const port = process.env.PORT || 5000;
-app.listen(port, () =>{
-    console.log(`Server started on port: ${port}`);
+var port = process.env.PORT || '5000'
+app.listen(port, err => {
+    if (err)
+        throw err
+    console.log('Server listening on port', port)
 });
